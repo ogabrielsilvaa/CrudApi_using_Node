18b528bcacb7ef051a8f44321efe0283e17c0a15
ogabrielsilvaa <ogabrieusiuva@gmail.com>
2026-03-18 08:51:36 -0300

    Documentação Swagger da rota de login.

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -5,10 +5,58 @@ const db = require('../database/models');
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /auth/login:
+ *  post:
+ *    summary: Autentica um usuário e retorna um token JWT
+ *    tags: [Autenticação]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            required:
+ *              - email
+ *              - password
+ *            properties:
+ *              email:
+ *                type: string
+ *                example: arrascaeta@flamengo.com
+ *              password:
+ *                type: string
+ *                example: 123
+ *    responses:
+ *      200:
+ *        description: Login realizado com sucesso
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                tokenType:
+ *                  type: string
+ *                  example: Bearer
+ *                accessToken:
+ *                  type: string
+ *                  description: Token JWT gerado
+ *                  example: eyJhbGciOiJIUzI1NiIsInR5cCI...
+ *                expiresIn:
+ *                  type: string
+ *                  description: Tempo de expiração do token
+ *                  example: 1d
+ *      400:
+ *        description: Dados incompletos (E-mail ou senha ausentes)
+ *      401:
+ *        description: Credenciais inválidas (Usuário não encontrado ou senha incorreta)
+ *      500:
+ *        description: Erro interno no servidor
+ */
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
   if (!email || !password)
-    return res.status(400).json({ message: "Informe username e password." });
+    return res.status(400).json({ message: "Informe email e password." });
 
   try {
     const user = await db.Usuario.findOne({ where: { email: email } });
